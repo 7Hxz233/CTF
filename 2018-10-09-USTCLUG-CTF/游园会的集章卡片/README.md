6f1ac3feba8e29c7957b881c1b25f51ead38d4ff
Wang Yihang <wangyihanger@gmail.com>
2018-10-13 12:46:07 +0800

    [+] 2018-10-09 USTCLUG CTF

---
+++ 2018-10-09-USTCLUG-CTF/游园会的集章卡片/README.md
@@ -0,0 +1,24 @@
+#### 游园会的集章卡片  
+
+```  
+<p style="color:red;">提示：flag 仅由 <code>0154agflPYHMGRCNE_{}</code> 这些字符组成。</p>
+<p>我们经常被问一个问题：你们是从什么时候开始为 Hackergame 出题的？</p>
+<p>其实我们全年都在出题，命题组成员来自各种各样的专业，在各种各样的地方，有各种各样不同的工作，闲暇之余<del>（或者工作不饱和的时候，这个最好删掉）</del>我们最大的乐趣就是互相出题给对方玩。</p>
+<p>为了找到有趣的题目，我们需要源源不断的灵感，不管是在巴蜀人家二楼包厢 LUD（<a href="https://lug.ustc.edu.cn/wiki/lug/events/start?s[]=lud">注 1</a>） 时，还是在北京食宝街的分米鸡，甚至是在武汉开往合肥的 D2256 列车上，我们会把讨论到的有趣的想法记录下来，作为下一次 Hackergame 的题目。</p>
+<p>比如前几天，中国科学技术大学学生 Linux 用户协会在中区游园会摆摊招新（<a href="https://lug.ustc.edu.cn/wiki/lug/contribute">注 2</a>），发现学校居然为每个参加游园会的同学准备了一张精美的集章卡片：</p>
+<p><img src="/file/e7c9fd503e3a4d3c1eb3db8907d91a200b39558fa492df294ccc1c3cc25ffd99/card.png" width="350px" /></p>
+<p>到每个社团的摊位上收集盖章，到达一定数量就有礼品赠送。</p>
+<p>突然一位同学灵机一动，不如写上 flag 然后撕碎！</p>
+<p>附件就是撕碎的 flag，相信对中国科学技术大学校徽了如指掌的你很快就能将它还原。</p>  
+```  
+#### Hint:  
+
+``` 
+flag{...}  
+``` 
+#### Information:  
+
+* File: [fragments.zip](files/fragments.zip)  
+
+* Soved: 1053  
+
---
+++ spider/ustclug.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json
+import requests
+import sys
+import os
+
+website = 'https://hack.lug.ustc.edu.cn'
+
+def parse_challenges(folder, challenges):
+    for challenge in challenges:
+        name = challenge['name']
+        print("[+] Parsing %s" % (name))
+        path = '%s/%s' % (folder, name)
+
+        try:
+            os.makedirs(path)
+        except Exception as e:
+            pass
+        markdown = ''
+        markdown += '#### %s  \n\n' % (name)
+        markdown += '```  \n'
+        markdown += '%s  \n' % (challenge['detail'])
+        markdown += '```  \n'
+        markdown += '#### Hint:  \n\n'
+        markdown += '``` \n'
+        markdown += '%s  \n' % (challenge['prompt'])
+        markdown += '``` \n'
+        markdown += '#### Information:  \n\n'
+        url = challenge['url']
+        if url.startswith("/file/"):
+            file_path = '%s/files' % (path)
+            try:
+                os.makedirs(file_path)
+            except Exception as e:
+                pass
+            download_link = '%s%s' % (website, url)
+            download(download_link, file_path)
+            filename = url.split("/")[-1]
+            markdown += '* File: [%s](files/%s)  \n\n' % (filename, filename)
+        else:
+            markdown += '* Url: %s  \n\n' % (challenge['url'])
+        markdown += '* Soved: %d  \n\n' % (challenge['user_solved'])
+        with open('%s/README.md' % path, 'w') as f:
+            f.write(markdown)
+
+def download(url, folder):
+    print("[+] Download %s to %s" % (url, folder))
+    filename = url.split("/")[-1]
+    content = requests.get(url).content
+    path = '%s/%s' % (folder, filename)
+    with open(path, 'wb') as f:
+        f.write(content)
+
+def main():
+    json_file = sys.argv[1]
+    folder = sys.argv[2]
+    challenges = json.load(open(json_file))
+    parse_challenges(folder, challenges)
+
+if __name__ == '__main__':
+    main()
